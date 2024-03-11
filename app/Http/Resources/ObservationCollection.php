ac3de5035499052a10b9e665367ba28d16fa7563
Alice Corry <alicecorry@icloud.com>
2024-03-11 10:32:05 +0000

    Only showing logged in user's observations when logged in. Showing message if no observations.

--- app/Http/Resources/ObservationCollection.php
+++ app/Http/Resources/ObservationCollection.php
@@ -3,13 +3,28 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Facades\Auth;
 
 class ObservationCollection extends ResourceCollection
 {
     public function toArray($request)
     {
+        $user = Auth::user();
+
+        $filteredObservations = $this->collection->filter(function ($observation) use ($user) {
+            return $observation->user_id === $user->id;
+        });
+
+        if ($filteredObservations->isEmpty()) {
+            return [
+                'message' => 'No observations found!',
+                'version' => '1.0.0',
+                'author' => 'Alice Corry'
+            ];
+        }
+
         return [
-            'data' => $this->collection,
+            'data' => $filteredObservations,
             'version' => '1.0.0',
             'author' => 'Alice Corry'
         ];
