48da7bde497da8438a7a8100307348b944cfbcfc
Alice Corry <alicecorry@icloud.com>
2024-03-19 12:31:19 +0000

    Editing the routes

--- routes/api.php
+++ routes/api.php
@@ -11,23 +11,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // Routes for authentication
-Route::post('/auth/register', [AuthController::class, 'register']); // Register a new user
-Route::post('/auth/login', [AuthController::class, 'login']); // Log in a user
+Route::post('/register', [AuthController::class, 'register']); // Register a new user
+Route::post('/login', [AuthController::class, 'login']); // Log in a user
 
 // Route to get observations (without authentication)
-Route::get('/auth/observations', [ObservationController::class, 'index']);
+Route::get('/observations', [ObservationController::class, 'index']);
 
 // Routes that require authentication
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/auth/user', [AuthController::class, 'user']); // Get authenticated user's details
-    Route::get('/auth/logout', [AuthController::class, 'logout']); // Log out user
+    Route::get('/user', [AuthController::class, 'user']); // Get authenticated user's details
+    Route::get('/logout', [AuthController::class, 'logout']); // Log out user
 
-    Route::get('/auth/observations/user', [ObservationController::class, 'indexByUser']); // Get observations by user
+    Route::get('/dashboard/observations/user', [ObservationController::class, 'indexByUser']); // Get observations by user
 
-    Route::get('/auth/observations/{uuid}', [ObservationController::class, 'showByUUID']); // Show a specific observation by UUID
-    Route::delete('/auth/observations/{uuid}', [ObservationController::class, 'destroyByUUID']); // Delete an observation by UUID
-    Route::put('/auth/observations/{uuid}', [ObservationController::class, 'updateByUUID']); // Update an observation by UUID
-    Route::get('/auth/observations/body/{id}', [ObservationController::class, 'indexByBodyAndUser']); // Get observations by body and user
-    Route::post('/auth/observations', [ObservationController::class, 'store']); // Store a new observation
+    Route::get('/dashboard/observations/{uuid}', [ObservationController::class, 'showByUUID']); // Show a specific observation by UUID
+    Route::delete('/dashboard/observations/{uuid}', [ObservationController::class, 'destroyByUUID']); // Delete an observation by UUID
+    Route::put('/dashboard/observations/{uuid}', [ObservationController::class, 'updateByUUID']); // Update an observation by UUID
+    Route::get('/dashboard/observations/body/{id}', [ObservationController::class, 'indexByBodyAndUser']); // Get observations by body and user
+    Route::post('/dashboard/observations', [ObservationController::class, 'store']); // Store a new observation
 
 });
